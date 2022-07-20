80e0f10b87a6091c0f4f63e9e2cdca4cf84f7760
Marcin Krystianc <marcin.krystianc@gmail.com>
2022-07-20 14:38:07 +0200

    fix populate

--- csharp/CassandraTesting/PopulateCommand.cs
+++ csharp/CassandraTesting/PopulateCommand.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using Cassandra;
 using Spectre.Console.Cli;
 
@@ -12,11 +13,14 @@ public class PopulateCommand : AsyncCommand<PopulateSettings>
         var ps = session.Prepare("INSERT INTO my_keyspace.my_table (partition_id, row_id, payload) VALUES  (?, ?, ?)");
         ps.SetConsistencyLevel(ConsistencyLevel.LocalQuorum);
         ps.SetIdempotence(false);
-            
+
         for (var partition = 0; partition < settings.NumberOfPartitions; partition++)
-        for (var i = 0; i < 10 << partition; i++)
         {
-            await session.ExecuteAsync(ps.Bind(partition, i, $"test_{partition}_{i}")).ConfigureAwait(false);
+            int numberOfRows = Convert.ToInt32(Math.Pow(10, partition));
+            for (var i = 0; i < numberOfRows; i++)
+            {
+                await session.ExecuteAsync(ps.Bind(partition, i, $"test_{partition}_{i}")).ConfigureAwait(false);
+            }
         }
 
         return 0;

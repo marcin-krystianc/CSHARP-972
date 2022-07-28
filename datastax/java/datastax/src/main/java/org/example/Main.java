75592b4050d2ea0c83e8710faf20b2d42039e3f8
Marcin Krystianc <marcin.krystianc@gmail.com>
2022-07-28 13:58:19 +0200

    Main class

--- datastax/java/datastax/src/main/java/org/example/Main.java
+++ datastax/java/datastax/src/main/java/org/example/Main.java
@@ -5,7 +5,7 @@ import java.net.InetSocketAddress;
 import java.util.concurrent.*;
 import java.util.concurrent.atomic.*;
 
-public class JavaAsyncExample {
+public class Main {
     private static boolean isRunning = true;
 
     public static void main(String[] args) throws Exception {

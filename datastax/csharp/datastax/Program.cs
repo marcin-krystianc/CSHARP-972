021c4ffe65883b4daea45aa00830b5bc696c3106
Marcin Krystianc <marcin.krystianc@gmail.com>
2022-07-28 13:37:58 +0200

    parse args

--- datastax/csharp/datastax/Program.cs
+++ datastax/csharp/datastax/Program.cs
@@ -18,17 +18,19 @@ namespace cassandra_csharp_test
                 .WithPort(9042)
                 .WithLoadBalancingPolicy(new TokenAwarePolicy(new DCAwareRoundRobinPolicy("datacenter1")))
                 .Build();
+
+            var tasksCount = Convert.ToInt32(args[1]);
+            var partition = Convert.ToInt32(args[2]);
             
             var session = cluster.Connect("my_keyspace");
             var ps = session.Prepare($"SELECT * FROM my_table where partition_id = ?");
             ps.SetConsistencyLevel(ConsistencyLevel.One);
             ps.SetIdempotence(false);
 
-            const int TASK_COUNT = 32;
-            Task<long>[] tasks = new Task<long>[TASK_COUNT];
-            for (var i = 0; i < TASK_COUNT; i++)
+            Task<long>[] tasks = new Task<long>[tasksCount];
+            for (var i = 0; i < tasksCount; i++)
             {
-                tasks[i] = worker(session, ps);
+                tasks[i] = worker(session, ps, partition);
             }
             Stopwatch stopWatch = Stopwatch.StartNew();
             Thread.Sleep(60000);
@@ -36,7 +38,7 @@ namespace cassandra_csharp_test
             Task.WaitAll(tasks);
             stopWatch.Stop();
             long count = 0;
-            for (var i = 0; i < TASK_COUNT; i++)
+            for (var i = 0; i < tasksCount; i++)
             {
                 count += tasks[i].Result;
             }
@@ -44,11 +46,11 @@ namespace cassandra_csharp_test
             Console.WriteLine("Rate: {0} rows/second", rate);
         }
 
-        static async Task<long> worker(ISession session, PreparedStatement ps)
+        static async Task<long> worker(ISession session, PreparedStatement ps, int partition)
         {
             long subtotal = 0;
             while (isRunning) {
-                var rs = await session.ExecuteAsync(ps.Bind(4)).ConfigureAwait(false);
+                var rs = await session.ExecuteAsync(ps.Bind(partition)).ConfigureAwait(false);
                 foreach (var row in rs)
                 {
                     subtotal++;
